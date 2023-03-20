ea4ace4f5c06eeaccd53006ab3da0273ebc215d3
Michael Mathias <michaelscodingjourney@gmail.com>
2023-03-20 14:55:07 -0700

    Passed five tests for first class functions

---
+++ index.js
@@ -0,0 +1,9 @@
+const receivesAFunction = (callback) => callback();
+
+const returnsANamedFunction = () => {
+    return aNamedFunction = () => "I am a named function!";
+};
+
+const returnsAnAnonymousFunction = () => {
+    return () => "I am an anonymous function!";
+}
